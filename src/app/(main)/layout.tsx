f2fb5ca84a43c4a715de2de45ccb3dca290ff3f5
Tanvir Ahmed <tanvir007arh@gmail.com>
2026-06-15 23:15:49 +0600

    fix: home page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,8 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
